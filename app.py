1d5add9f5147d7c3fcbed92a6e924f61109a5c4f
Sneeg15 <53247710+Sneeg15@users.noreply.github.com>
2023-02-24 17:50:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,4 @@
 import json
-import boto3
-
-s3 = boto3.client('s3')
-
-def lambda_handler(event, context):
-    bucket = 'test-bucket-sneeg'
-    upload = {}
-    upload['transaction']= '1234'
-    upload['type']= 'purchase'
-    upload['amount']= 1000
-    upload['customerID']= 'CID1234'
-
-    filename = upload['customerID'] + '.json'
-
-    uploadbytestream= bytes(json.dumps(upload).encode('UTF-8'))
-
-    s3.put_object(Bucket= bucket, Key=filename, Body=uploadbytestream)
-
-    print('put complete')
-
-
-
-import json
 import requests
 
 def lambda_handler(event, context):
